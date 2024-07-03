a093e150e0068efe5c899141b1c9a9e7cf1598be
luckysetiawan <luckysetiawan0802@gmail.com>
2024-07-03 17:24:06 +0700

    implement erc721 with tests

--- test/TokenMaster.js
+++ test/TokenMaster.js
@@ -1,5 +1,32 @@
 const { expect } = require("chai");
 
-describe("TokenMaster", () => {
+const NAME = "TokenMaster";
+const SYMBOL = "TM";
 
+describe("TokenMaster", () => {
+  let tokenMaster;
+  let deployer, buyer;
+
+  beforeEach(async () => {
+    // Setup accounts
+    [ deployer, buyer ] = await ethers.getSigners();
+
+    // Deploy Contract
+    const TokenMasterFactory = await ethers.getContractFactory("TokenMaster");
+    tokenMaster = await TokenMasterFactory.deploy(NAME, SYMBOL);
+  })
+
+  describe("Deployment", () => {
+    it("Sets the name", async () => {
+      expect(await tokenMaster.name()).to.equal(NAME);
+    })
+
+    it("Sets the symbol", async () => {
+      expect(await tokenMaster.symbol()).to.equal(SYMBOL);
+    })
+
+    it("Sets the owner", async () => {
+      expect(await tokenMaster.owner()).to.equal(deployer.address);
+    })
+  })
 })
