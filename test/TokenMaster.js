654b6cd3b0569e0c7e1b6f549eee4dbe82272847
luckysetiawan <luckysetiawan0802@gmail.com>
2024-07-03 17:56:42 +0700

    add list occasion with tests

--- test/TokenMaster.js
+++ test/TokenMaster.js
@@ -3,6 +3,13 @@ const { expect } = require("chai");
 const NAME = "TokenMaster";
 const SYMBOL = "TM";
 
+const OCCASION_NAME = "ETH Texas";
+const OCCASION_COST = ethers.parseUnits('1', 'ether');
+const OCCASION_MAX_TICKETS = 100;
+const OCCASION_DATE = "Apr 27";
+const OCCASION_TIME = "10:00AM CST";
+const OCCASION_LOCATION = "Austin, Texas";
+
 describe("TokenMaster", () => {
   let tokenMaster;
   let deployer, buyer;
@@ -14,6 +21,17 @@ describe("TokenMaster", () => {
     // Deploy Contract
     const TokenMasterFactory = await ethers.getContractFactory("TokenMaster");
     tokenMaster = await TokenMasterFactory.deploy(NAME, SYMBOL);
+
+    // Add an occasion
+    const transaction = await tokenMaster.connect(deployer).list(
+      OCCASION_NAME,
+      OCCASION_COST,
+      OCCASION_MAX_TICKETS,
+      OCCASION_DATE,
+      OCCASION_TIME,
+      OCCASION_LOCATION
+    );
+    await transaction.wait();
   })
 
   describe("Deployment", () => {
@@ -27,6 +45,24 @@ describe("TokenMaster", () => {
 
     it("Sets the owner", async () => {
       expect(await tokenMaster.owner()).to.equal(deployer.address);
+    })
+  })
+
+  describe("Occasions", () => {
+    it('Returns occasions attributes', async () => {
+      const occasion = await tokenMaster.getOccasion(1);
+
+      expect(occasion.id).to.be.equal(1);
+      expect(occasion.name).to.be.equal(OCCASION_NAME);
+      expect(occasion.cost).to.be.equal(OCCASION_COST);
+      expect(occasion.tickets).to.be.equal(OCCASION_MAX_TICKETS);
+      expect(occasion.date).to.be.equal(OCCASION_DATE);
+      expect(occasion.time).to.be.equal(OCCASION_TIME);
+      expect(occasion.location).to.be.equal(OCCASION_LOCATION);
+    })
+
+    it("Updates occasions count", async () => {
+      expect(await tokenMaster.totalOccasions()).to.equal(1);
     })
   })
 })
