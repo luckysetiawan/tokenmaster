d3d72726fb76494d4b2099bf45f3910d55f08e0a
luckysetiawan <luckysetiawan0802@gmail.com>
2024-07-05 14:58:18 +0700

    integrate to blockchain

--- src/App.js
+++ src/App.js
@@ -14,9 +14,35 @@ import TokenMaster from './abis/TokenMaster.json'
 import config from './config.json'
 
 function App() {
+  const [provider, setProvider] = useState(null);
   const [account, setAccount] = useState(null);
 
+  const [tokenMaster, setTokenMaster] = useState(null);
+  const [occasions, setOccasions] = useState(null);
+
+  const [occasion, setOccasion] = useState({});
+  const [toggle, setToggle] = useState(false);
+
   const loadBlockchainData = async () => {
+    const provider = new ethers.BrowserProvider(window.ethereum);
+    setProvider(provider);
+    const network = await provider.getNetwork();
+    const address = config[network.chainId].TokenMaster.address;
+    
+    const tokenMaster = new ethers.Contract(address, TokenMaster, provider);
+    setTokenMaster(tokenMaster);
+
+    // Get Occasions Data
+    const totalOccasions = await tokenMaster.totalOccasions();
+    const occasions = [];
+
+    for (let i = 1; i <= totalOccasions; i++) {
+      const occasion = await tokenMaster.getOccasion(i);
+      occasions.push(occasion);
+    }
+    setOccasions(occasions);
+    console.log(occasions);
+
     // Refresh Account
     window.ethereum.on('accountsChanged', async () => {
       let accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
@@ -37,6 +63,32 @@ function App() {
         <h2 className="header__title"><strong>Event</strong> Tickets</h2>
       </header>
 
+      <Sort />
+
+      <div className='cards'>
+        {occasions && occasions.map((occasion, index) => (
+          <Card
+            occasion={occasion}
+            id={index + 1}
+            tokenMaster={tokenMaster}
+            provider={provider}
+            account={account}
+            toggle={toggle}
+            setToggle={setToggle}
+            setOccasion={setOccasion}
+            key={index}
+          />
+        ))}
+      </div>
+
+      {toggle && (
+        <SeatChart
+          occasion={occasion}
+          tokenMaster={tokenMaster}
+          provider={provider}
+          setToggle={setToggle}
+        />
+      )}
     </div>
   );
 }
